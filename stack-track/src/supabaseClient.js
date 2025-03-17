52e05548a870940202651c4ea07b8167cb61c373
Vincenzo <vincenzo.scotto001@gmail.com>
2025-03-16 20:30:27 -0400

    changed to make dynamic

--- stack-track/src/supabaseClient.js
+++ stack-track/src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Replace with your actual Supabase URL and anon key
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
