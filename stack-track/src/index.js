1fdaa1fc3daf7d5408c2c9afd6abdf891c32bb2f
Vincenzo <vincenzo.scotto001@gmail.com>
2025-03-30 22:14:52 -0400

    app

--- stack-track/src/index.js
+++ stack-track/src/index.js
@@ -1,7 +1,10 @@
+// Update your index.js file to register the service worker
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +12,29 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register the service worker for PWA functionality
+serviceWorkerRegistration.register({
+  onUpdate: registration => {
+    // Show a notification to the user when a new version is available
+    const updateNotification = document.createElement('div');
+    updateNotification.className = 'update-notification';
+    updateNotification.innerHTML = `
+      <div class="update-message">
+        <p>A new version of StackTrack is available!</p>
+        <button id="update-button">Update Now</button>
+      </div>
+    `;
+    document.body.appendChild(updateNotification);
+
+    // Add click handler for the update button
+    document.getElementById('update-button').addEventListener('click', () => {
+      if (registration && registration.waiting) {
+        // Send a message to the waiting service worker
+        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      }
+      // Reload the page to activate the new version
+      window.location.reload();
+    });
+  }
+});
